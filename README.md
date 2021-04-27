58692855043fa758f19b60dd0d701e8227809f66
hilalhmdy <mhdhilalhmdy15@gmail.com>
2021-04-27 14:30:23 +0700

    add flask

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tubes-3-stima
+
+## cara gunain
+```
+pip install -r requirements.txt
+run app.py `python app.py`.
+```
---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from os import path
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+
+    userText = request.args.get('msg')
+
+    return feature.process(userText)
+
+if __name__ == "__main__":
+    app.run(debug = True)
